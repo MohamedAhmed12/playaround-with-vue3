30be0568e3eb67cc95e61094da6d36e59934efc3
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-12-20 01:53:06 +0200

    1-absolute-basics

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      greeting: "Hello World",
+    };
+  },
+
+  mounted(){
+    setTimeout(()=>{
+        this.greeting = 'changed'
+    }, 3000)
+  }
+};
+</script>
+
+<template>
+  <input type="text" v-model="greeting" />
+  <p>
+    {{ greeting }}
+  </p>
+</template>
+
+<style scoped>
+</style>
