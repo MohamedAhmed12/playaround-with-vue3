4d7f6ce655a1872eba3723f5b33b6692d2cb216b
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-12-30 07:15:28 +0200

    feat(member): add-member-modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,61 @@
+<script setup>
+defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <div class="modal-mask" v-if="show">
+    <div class="modal-container">
+      <header>
+        <slot name="header">Default header</slot>
+      </header>
+
+      <div>
+        <slot>Default body</slot>
+      </div>
+
+      <footer class="modal-footer flex justify-start">
+        <slot name="footer">
+          <button @click="$emit('close')">Default Close</button>
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-mask {
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.4);
+  display: grid;
+  place-items: center;
+}
+.modal-container {
+  background: white;
+  padding: 1rem;
+  color: black;
+  width: 80vw;
+  max-width: 500px;
+  border-radius: 7px;
+}
+.modal-footer {
+  border-top: 1px solid #ddd;
+  margin-top: 1rem;
+  padding-top: 0.5rem;
+  font-size: 0.8rem;
+}
+.modal-footer button {
+  background: #ddd;
+  padding: 0.25rem 0.75rem;
+  border-radius: 20px;
+}
+.modal-footer button:hover {
+  background: #c8c8c8;
+}
+</style>
+
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,21 +1,40 @@
 <script setup>
-// import team from "@/team.json";
 import TeamHeader from "@/components/Team/TeamHeader.vue";
 import TeamMembers from "@/components/Team/TeamMembers.vue";
 import TeamFooter from "@/components/Team/TeamFooter.vue";
+import Modal from "@/components/Modal.vue";
 import { useTeamStore } from "@/stores/teamStore.js";
+import { ref } from "vue";
 
 const team = useTeamStore();
-
 team.fill();
+
+const showModal = ref(false);
 </script>
 
 <template>
-  <TeamHeader />
+  <TeamHeader @add="showModal = true" />
 
   <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
     <TeamMembers />
   </div>
 
   <TeamFooter />
+
+  <Modal :show="showModal" @close="showModal = false">
+    <template #header>
+      <div class="mb-5">override header</div>
+    </template>
+
+    <template #default>
+      <P>Need to add new member to your team?</P>
+
+      <form class="mt-6">
+        <div class="flex">
+          <input type="email" placeholder="Email Address..."  class="flex-1"/>
+          <button>Add</button>
+        </div>
+      </form>
+    </template>
+  </Modal>
 </template>
